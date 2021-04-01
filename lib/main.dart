5444239b6af96298375a1666dea608739434d41c
buxexalg <lucasgralcantara@gmail.com>
2021-04-01 17:54:13 -0300

    feat: add class exercise structure

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,40 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+        home: Column(children: [
+      Expanded(
+          child: Container(
+        child: Column(children: [
+          Container(
+            color: Colors.white,
+            height: 80,
+            margin: EdgeInsets.all(40),
+          ),
+          Container(
+            color: Colors.white,
+            height: 80,
+            margin: EdgeInsets.all(40),
+          ),
+          Container(
+            color: Colors.white,
+            height: 80,
+            margin: EdgeInsets.all(40),
+          )
+        ]),
+      )),
+      Container(
+        height: 100,
+        color: Colors.blue,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Container(height: 50, width: 50, color: Colors.green),
+            Container(height: 50, width: 50, color: Colors.yellow),
+            Container(height: 50, width: 50, color: Colors.red),
+          ],
+        ),
+      )
+    ]));
   }
 }
